67710e87c14323d48f82d89bcf69068087b8ed50
Raja244 <rajasaha480@gmail.com>
2023-12-13 12:32:31 +0530

    Fibonacci Series

---
+++ fibonacci_Series.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=20,fbb,a=0,b=1,i=0;
+    cout<<a<<" ";
+    
+    while (i<n)
+    {   
+        fbb=a+b;
+        b=a;
+        a=fbb;
+        cout<<fbb<<" ";
+        i++;
+    }
+    
+}
